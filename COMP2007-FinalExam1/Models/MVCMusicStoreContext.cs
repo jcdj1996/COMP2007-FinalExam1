2dfd459cc3067908c592eb65f3b72dec6da32a25
James <James@LENOVO-PC>
2016-08-18 10:35:06 -0400

    Controller Views working

--- COMP2007-FinalExam1/Models/MVCMusicStoreContext.cs
+++ COMP2007-FinalExam1/Models/MVCMusicStoreContext.cs
@@ -8,19 +8,28 @@ namespace COMP2007_FinalExam1.Models
     public partial class MVCMusicStoreContext : DbContext
     {
         public MVCMusicStoreContext()
-            : base("name=MVCMusicStoreConnection")
+            : base("name=MVCMusicStoreContext")
         {
         }
 
+        public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
         public virtual DbSet<Album> Albums { get; set; }
         public virtual DbSet<Artist> Artists { get; set; }
         public virtual DbSet<Cart> Carts { get; set; }
         public virtual DbSet<Genre> Genres { get; set; }
         public virtual DbSet<OrderDetail> OrderDetails { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<database_firewall_rules> database_firewall_rules { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<database_firewall_rules>()
+                .Property(e => e.start_ip_address)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<database_firewall_rules>()
+                .Property(e => e.end_ip_address)
+                .IsUnicode(false);
         }
     }
 }
